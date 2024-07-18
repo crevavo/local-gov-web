aef24d373d441670c20b6c601a159b90c6de3edd
Hyougo Yuuki <yuuki.hyougo@gmail.com>
2024-07-18 23:45:43 +0900

    一旦仮ページ

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@digital-go-jp/tailwind-theme-plugin')],
+}
+
